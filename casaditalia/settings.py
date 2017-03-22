18475c47b35e85e9f994514e3c3499dda72e6c4c
Júnior Carvalho <joseadolfojr@gmail.com>
2017-03-22 16:25:24 -0300

    settings

--- casaditalia/settings.py
+++ casaditalia/settings.py
@@ -123,10 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 admin.site.site_header = 'Mala Direta'
